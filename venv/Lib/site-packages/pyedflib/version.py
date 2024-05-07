dc6ed155025c711163e951e44f9fb683b179b511
tomasccpt <tomascurralo2014@gmail.com>
2024-05-07 17:01:13 +0100

    Automating file reading from dataset

---
+++ venv/Lib/site-packages/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.37'
+version = '0.1.37'
+full_version = '0.1.37'
+git_revision = '91728a201a0daa5cb5bfd15a0533e7643ca52203'
+release = True
+
+if not release:
+    version = full_version
